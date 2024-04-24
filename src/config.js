32754767c11238386fd44c1c5b1da358199d6243
minhduz <minhducoverwatch@gmail.com>
2024-04-24 09:55:47 +0700

    update-url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const api_endpoint = "http://localhost:3000/api";
 
-export const api_endpoint = "https://atn-toy-api.onrender.com/api";
+export const api_endpoint = "https://atn-toy-api-t253.onrender.com/api";
